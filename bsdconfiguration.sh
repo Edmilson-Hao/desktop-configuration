b9a2fbb81710bb943b6ff640d8f876c0df3d318b
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2022-11-06 17:32:53 -0300

    Add files via upload

---
+++ bsdconfiguration.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#Needed pkgs
+pkg install xorg i3-gaps ufetch neofetch scrot feh i3status i3lock nano micro doas lxappearance leafpad rxvt-unicode xterm setxkbd
+
+#configure doas
+echo "permit nopass edmilson" >> /usr/local/etc/doas.conf
+
+#add i3 to .xinitrc
+echo "/urc/local/bin/i3" >> /home/edmilson/.xinitrc
+
+#add startx to .shrc
+echo "r=`top | grep Xorg`\nif [ "$r" ]; then\n	ufetch\nelse\n    echo "Xorg is not running."\n    startx &\nfi" >> /home/edmilson/.shrc
+
+#move wallpaper
+mkdir /home/edmilson/wallpaper
+mv wallpaper.jpg /home/edmilson/wallpaper/wallpaper.jpg
+
+#move i3status.conf
+mv i3status.conf /usr/local/etc/i3status.conf
+
+#mv i3 config file
+mv config /home/edmilson/.config/i3/config
+
+#mv .Xresources
+mv .Xresources /home/edmilson/.Xresources
