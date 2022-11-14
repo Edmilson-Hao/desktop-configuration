d24cc58dd583cf392b8fd3e7da7c5b61929fd466
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2022-11-14 13:30:45 -0300

    Update bsdconfiguration.sh

--- FreeBSD/bsdconfiguration.sh
+++ FreeBSD/bsdconfiguration.sh
@@ -1,20 +1,43 @@
 #!/bin/sh
 
+userName=''
+#Get username
+echo "What is your username? "
+echo "Enter the user name: "  
+read userName
+
+clear
+
 #Needed pkgs
-pkg install xorg i3-gaps ufetch neofetch scrot feh i3status i3lock nano micro doas lxappearance leafpad rxvt-unicode xterm setxkbmap papirus-icon-theme terminus-font pcmangm compton htop font-awesome
+echo "Installing packages."
+pkg install xorg i3-gaps ufetch neofetch scrot feh i3status i3lock nano micro doas lxappearance leafpad rxvt-unicode xterm setxkbmap papirus-icon-theme terminus-font pcmanfm compton htop font-awesome
 
 #configure doas
-echo "permit nopass edmilson" >> /usr/local/etc/doas.conf
+echo "Configuring dotfiles."
+[! -f "/usr/local/etc/doas.conf"] && touch /usr/local/etc/doas.conf
+echo "permit nopass $userName" > /usr/local/etc/doas.conf && echo "doas -> ok"
 
 #add i3 to .xinitrc
-echo "/urc/local/bin/i3" >> /home/edmilson/.xinitrc
+[ ! -f "/home/$userName/.xinitrc" ] && touch /home/$userName/.xinitrc
+echo "/urc/local/bin/i3" >> /home/edmilson/.xinitrc && echo "add i3 to .xinitrc -> ok"
 
 #add startx to .shrc
-echo "r=`top | grep Xorg`\nif [ "$r" ]; then\n	ufetch\nelse\n    echo "Xorg is not running."\n    startx &\nfi" >> /home/edmilson/.shrc
+[ ! -f "/home/$userName/.shrc" ]  && touch /home/$userName/.shrc
+echo "r=`top | grep Xorg`\nif [ "$r" ]; then\n	ufetch\nelse\n    \n    startx &\nfi" >> /home/edmilson/.shrc
+
+echo "if pgrep -x "Xorg" > /dev/null
+then
+        ufetch
+else
+        echo "Xorg is not running."
+        startx &
+fi" >> /home/$userName/.shrc && echo "Add test to check if X is running an launch if not"
 
 #move wallpaper
-mkdir /home/edmilson/wallpaper
-mv wallpaper.jpg /home/edmilson/wallpaper/wallpaper.jpg
+[ ! -d "/home/$userName/.wallpaper" ] && mkdir /home/$userName/.wallpaper
+mv wallpaper.jpg /home/$userName/.wallpaper/wallpaper.jpg && echo "Moved wallpaper to correct folder."
+
+#-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-##-#-#-#-#-#-#-#
 
 #move i3status.conf
 mv i3status.conf /usr/local/etc/i3status.conf
