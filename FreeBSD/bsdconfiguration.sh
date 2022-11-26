421fa724c8c976181ccecdb608649da50bd8c3e2
Edmilson-Hao <edmilsonwp@gmail.com>
2022-11-25 21:10:29 -0300

    graphic driver selector

--- FreeBSD/bsdconfiguration.sh
+++ FreeBSD/bsdconfiguration.sh
@@ -11,11 +11,11 @@ pkg install $neededPackages $neededPackages
 
 #configure doas
 echo "Configuring dotfiles."
-[! -f "/usr/local/etc/doas.conf"] && touch /usr/local/etc/doas.conf
+[[ ! -f "/usr/local/etc/doas.conf"]] && touch /usr/local/etc/doas.conf
 echo "permit nopass $userName" > /usr/local/etc/doas.conf && echo "doas -> ok"
 
 #add i3 to .xinitrc
-[ ! -f "/home/$userName/.xinitrc" ] && touch /home/$userName/.xinitrc
+[[ ! -f "/home/$userName/.xinitrc" ]] && touch /home/$userName/.xinitrc
 echo "/usr/local/bin/i3" >> /home/edmilson/.xinitrc && echo "add i3 to .xinitrc -> ok"
 
 #add startx to .shrc
@@ -30,14 +30,14 @@ else
 fi" >> /home/$userName/.shrc && echo "Add test to check if X is running an launch if not"
 
 #move wallpaper
-[ ! -d "/home/$userName/.wallpaper" ] && mkdir /home/$userName/.wallpaper
+[ ! -d "/home/$userName/.wallpaper" ]] && mkdir /home/$userName/.wallpaper
 mv wallpaper.jpg /home/$userName/.wallpaper/wallpaper.jpg && echo "Moved wallpaper to correct folder."
 
 #move i3status.conf
 mv i3status.conf /usr/local/etc/i3status.conf
 
 #mv i3 config file
-[ ! -d "/home/$userName/.config/i3" ] && /home/$userName/.config/i3
+[ ! -d "/home/$userName/.config/i3" ]] && /home/$userName/.config/i3
 mv config /home/$userName/.config/i3/config
 
 #mv .Xresources
